c7197ee44eee523cfee1131add49d21f631f85db
Cipher <tonycypher0@gmail.com>
2024-09-12 05:53:35 +0100

    styled App Component

---
+++ src/shared/components/Navigation/MainHeader.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./MainHeader.css";
+
+const MainHeader = (props) => {
+  return;
+};
+
+export default MainHeader;
--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,17 @@
 import React from "react";
+import UsersList from "../components/UsersList";
 
 const Users = () => {
-  return <h2>Users Works!</h2>;
+  const USERS = [
+    {
+      id: "u1",
+      name: "Tony Stack",
+      image:
+        "https://i.pinimg.com/564x/2c/dd/1b/2cdd1b72e1334717eed7374a42b39889.jpg",
+      places: 3,
+    },
+  ];
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
